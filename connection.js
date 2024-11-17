e439734a085a93fb877e11bfc9266982b3cee003
neytism <nateodnerolf@gmail.com>
2024-11-18 02:49:26 +0800

    - updated week 10

--- connection.js
+++ connection.js
@@ -1,11 +1,19 @@
+//install node js
+//install postgres
+//install pgAdmin 4
+//restart
+//npm install pg --save
+//node server.js
+//install postman for post
+
 const {Client} = require('pg');
 
 const client = new Client({
     host: "localhost",
     user: "postgres",
     post: 5432,
-    password: "password",
-    database: "postgres"
+    password: "postgress", //accidentaly added an extra s on my pc
+    database: "gamenetworking"
 });
 
 module.exports = client;
--- server.js
+++ server.js
@@ -10,12 +10,72 @@ app.listen(3000, ()=> {
 
 client.connect();
 
+//select rows
 app.get('/users', (req, res) => {
     client.query('SELECT * FROM users', (err, result)=> {
         if(!err){
             res.send(result.rows)
         }
     });
+    
+    client.end;
+});
 
+//select row
+app.get('/users/:id', (req, res) => {
+    client.query(`SELECT * FROM users WHERE id = ${req.params.id}`, (err, result)=> {
+        if(!err){
+            res.send(result.rows)
+        }
+    });
+    
+    client.end;
+});
+
+//insert
+app.post('/users', (req, res) => {
+    const user = req.body;
+    let insertQuery = `INSERT INTO users(username, password, email) VALUES('${user.username}','${user.password}','${user.email}')`;
+    
+    client.query(insertQuery , (err, result)=> {
+        if(!err){
+            res.send("Registration Successful!");
+        } else{
+            console.log(err.message);
+        }
+    });
+    
+    client.end;
+
+});
+
+//update
+app.put('/users/:id', (req, res) => {
+    const user = req.body;
+    let updateQuery = `UPDATE users SET username = '${user.username}', email = '${user.email}', password = '${user.password}' WHERE id = '${req.params.id}'`;
+    
+    client.query(updateQuery , (err, result)=> {
+        if(!err){
+            res.send("Password changed successfully!");
+        } else{
+            console.log(err.message);
+        }
+    });
+    
+    client.end;
+
+});
+
+//delete
+app.delete('/users/:id', (req, res) => {
+    let deleteQuery = `DELETE FROM users WHERE id = '${req.params.id}'`;
+    client.query(deleteQuery, (err, result)=> {
+        if(!err){
+            res.send(`User with id of ${req.params.id} deleted successfully!`)
+        } else{
+            console.log(err.message);
+        }
+    });
+    
     client.end;
 });
